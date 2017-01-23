7ba59baa7bd600252a7a839e23dfffa2df35fe11
Edwin Brower <edwin.brower@gmail.com>
2017-01-23 00:07:08 -0800

    Completed Part 1

--- src/underbar.js
+++ src/underbar.js
@@ -184,49 +184,6 @@
     });
 
     return reduced;
-
-/*
-function reduce(array, combine, start) {
-  var current = start;
-  for (var i = 0; i < array.length; i++)
-    current = combine(current, array[i]);
-  return current;
-}
-*/
-
-/*    Array.prototype.reduce = function(combiner, initialValue) {
-  var counter,
-    accumulatedValue;
-
-  // If the array is empty, do nothing
-  if (this.length === 0) {
-    return this;
-  }
-  else {
-    // If the user didn't pass an initial value, use the first item.
-    if (arguments.length === 1) {
-      counter = 1;
-      accumulatedValue = this[0];
-    }
-    else if (arguments.length >= 2) {
-      counter = 0;
-      accumulatedValue = initialValue;
-    }
-    else {
-      throw "Invalid arguments.";
-    }
-
-    // Loop through the array, feeding the current value and the result of
-    // the previous computation back into the combiner function until
-    // we've exhausted the entire array and are left with only one function.
-    while(counter < this.length) {
-      accumulatedValue = combiner(accumulatedValue, this[counter])
-      counter++;
-    }
-
-    return [accumulatedValue];
-  }
-}; */
   };
 
   // Determine if the array or object contains a given value (using `===`).
